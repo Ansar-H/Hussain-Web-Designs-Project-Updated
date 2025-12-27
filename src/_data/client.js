c670068cee08b5f31c7f1c4aebdd615acc10ccc0
Ansar-H <ansarh954@gmail.com>
2025-12-27 16:02:39 +0000

    Updated client.json with relevant information

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,17 +1,17 @@
 module.exports = {
-  name: "Southwest Prestige Hire",
-  email: "southwestprestigehire@gmail.com",
-  phoneForTel: "44-1234-567-890",
-  phoneFormatted: "(44) 1234-567890",
+  name: "Hussain Web Designs",
+  email: "info@hussainwebdesigns.com",
+  phoneForTel: "44-7442-724028",
+  phoneFormatted: "+44 7442 724028",
   address: {
     city: "Bristol",
-    country: "UK",
+    country: "GB",
     // Removed full address, state, and ZIP for privacy
   },
   socials: {
-    facebook: "https://www.facebook.com/",
-    instagram: "https://www.instagram.com/",
+    facebook: "https://www.facebook.com/Ansar786123",
+    instagram: "https://www.instagram.com/ansar.h_",
   },
   //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-  domain: "https://southwestprestigehire.co.uk",
+  domain: "https://hussainwebdesigns.co.uk",
 };
